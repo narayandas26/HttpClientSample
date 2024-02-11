aab5df2209144b3ae809b3db172e3af7db7f2755
narayandas26 <narayandas26@gmail.com>
2024-02-11 15:53:29 -0500

    Add project files.

---
+++ HttpClientSample/ApiClient/WeatherForecastClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientSample.ApiClient
+{
+    internal class WeatherForecastClient : IWeatherForecastClient
+    {
+        //private static readonly HttpClient _httpClient = new();
+        private readonly HttpClient _httpClient;
+
+        public WeatherForecastClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("WeatherForecast");
+        }
+
+        public async Task<string> GetWeatherForecast()
+        {
+            var response = await _httpClient.GetStringAsync($"forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
+            return response;
+        }
+    }
+}
---
+++ HttpClientSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using HttpClientSample.ApiClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+internal class Program
+{
+    //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        ServiceCollection services = new();
+        services.AddHttpClient();
+        services.AddHttpClient("WeatherForecast", httpClient => {
+            httpClient.BaseAddress = new Uri("https://api.open-meteo.com/v1/");
+            httpClient.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "HttpClientFactory");
+        });
+
+        services.AddScoped<IWeatherForecastClient, WeatherForecastClient>();
+
+        var provider = services.BuildServiceProvider();
+
+        var weatherForecastClient = provider.GetService<IWeatherForecastClient>();
+        var weatherTask = weatherForecastClient.GetWeatherForecast();
+        weatherTask.Wait();
+        var response = weatherTask.Result;
+
+        Console.WriteLine("Complete");
+    }
+}
